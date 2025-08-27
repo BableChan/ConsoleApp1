dcc3a6ee261840b339ab0e6e2b3d6f34dfc3751c
BableChan <rasmus.widang@gmail.com>
2025-08-27 10:41:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string bröd, fordon, destination;
+Console.WriteLine("säg ditt favorit fikabröd");
+bröd = Console.ReadLine();
+
+Console.WriteLine("säg din favorit offentliga transportation");
+fordon = Console.ReadLine();
+
+Console.WriteLine("var är din favoritplats?");
+destination = Console.ReadLine();
+
+Console.WriteLine($"du ska åka {bröd}{fordon} till {destination}");
+
+Console.ReadLine();
